fee64fe6fdbd39340277ae558c90311d613099e5
Alec <afriedhoff@live.com>
2016-08-23 10:54:41 -0400

    created draft json data... draft dotplot code...

--- data/Import, process.R
+++ data/Import, process.R
@@ -2,31 +2,61 @@ library("ggmap")
 library("dplyr")
 library("ggplot2")
 library("tidyr")
+library("jsonlite")
 
-#gci <- read.csv("~/Projects/Brookings/gci-summit-2016/data/all_data.csv", stringsAsFactors = FALSE, na.strings=c("","NA","na"))
+#gci <- read.csv("~/Projects/Brookings/gci-summit-2016/data/cluster_indicators_alec_063016.csv", stringsAsFactors = FALSE, na.strings=c("","NA","na"), fileEncoding="latin1")
 #ll <- geocode(paste0(gci$metro,", ",gci$country), "latlona")
 #gci2 <- cbind(ll, gci)
-#saveRDS(gci2, file="~/Projects/Brookings/gci-summit-2016/data/all_data.RDS")
+#saveRDS(gci2, file="~/Projects/Brookings/gci-summit-2016/data/slimmed_version.RDS")
 
-gci2 <- readRDS(file = "~/Projects/Brookings/gci-summit-2016/data/all_data.RDS")
-vars <- read.csv(file = "~/Projects/Brookings/gci-summit-2016/data/varnames.csv", na.strings=c("na",""),row.names=NULL, stringsAsFactors=FALSE)
+gci2 <- readRDS(file = "~/Projects/Brookings/gci-summit-2016/data/slimmed_version.RDS")
+#vars <- read.csv(file = "~/Projects/Brookings/gci-summit-2016/data/varnames.csv", na.strings=c("na",""),row.names=NULL, stringsAsFactors=FALSE)
 
-m <- reshape2::melt(gci2, id.vars=c("metro2","country", "label_cluster"), measure.vars=7:183)
+gci2$id <- paste0("m",1:nrow(gci2))
+
+gci3 <- gci2[c(26, 25, 4, 5, 6, 1, 2, 7:24)]
+
+
+
+#growth of fdi??
+#keyvars <- gci2[c(4,6,1,2,7,9,13,21,22,24,25,26,27,28,148,149,155,157,158,)]
+
+#gci2[1:10, c(148,150, 158)]
+
+#m <- reshape2::melt(gci2, id.vars=c("metro2","country", "label_cluster"), measure.vars=7:183)
 
 #write.csv(data.frame(vars=unique(m$variable)), file="~/Projects/Brookings/gci-summit-2016/data/varnames.csv", row.names = FALSE)
 
 #run summaries by variable and by variable crossed with label cluster
 
-#function to add z-score
+#function to compute z score and return result as numeric -- summarise fails with scaled vectors -- presumably because they contain additional attributes... or the mean method dispatches differently...
 scaleChunk <- function(chunk){
-  chunk$z <- scale(chunk$value)
-  return(chunk)
+  return(as.numeric(scale(chunk)))
 }
 mean_ <- function(chunk){return(mean(chunk, na.rm=TRUE))}
 median_ <- function(chunk){return(median(chunk, na.rm=TRUE))}
 
-zs <- m %>% group_by(variable) %>% do(scaleChunk(.))
-meanz <- zs %>% group_by(variable, label_cluster) %>% summarise(mean=mean(z), mean2=mean_(z), median=median(z), median2=median_(z))
+predicate <- function(x){return(is.numeric(x) & !is.integer(x))}
+zs <- gci3[c(1,2,9:25)] %>% mutate_if(predicate, scaleChunk)
+minmax <- zs[3:19] %>% summarise_all(funs(max=max(., na.rm=TRUE), min=min(., na.rm=TRUE))) 
+meanz <- zs[2:19] %>% group_by(label_cluster) %>% summarise_all(funs(mean(., na.rm=TRUE)))
+minmax <- meanz[2:18] %>% summarise_all(funs(max=max(., na.rm=TRUE), min=min(., na.rm=TRUE))) 
+min(unlist(minmax))
+max(unlist(minmax))
+meanz$id <- paste0("grpavg",meanz$label_cluster)
+
+meanvals <- gci3[c(2,9:25)] %>% group_by(label_cluster) %>% summarise_all(funs(mean(., na.rm=TRUE)))
+
+final <- list(vals=list(), z=list())
+final$vals$metros <- gci3
+final$vals$groups <- meanvals
+final$z$metros <- zs
+final$z$groups <- meanz
+
+finalJ <- toJSON(final, digits=5)
+
+writeLines(finalJ, con="~/Projects/Brookings/gci-summit-2016/data.json")
+
 
 ggplot(zs, aes(x=z)) + geom_histogram() + facet_wrap(~variable)
 ggplot(zs, aes(x=label_cluster, y=z)) + geom_point() + facet_wrap(~variable)
@@ -35,3 +65,19 @@ ggplot(zs, aes(x=variable, y=z)) + geom_point(colour="red", alpha="0.25") + geom
 
 ggmeans <- ggplot(meanz, aes(x=label_cluster, y=median2))
 ggmeans + geom_bar(stat="identity") + facet_wrap(~variable, scales="fixed") + scale_x_continuous(breaks=1:7) + geom_text(aes(y=0, label=label_cluster), vjust=0, colour="#ffffff")
+
+
+#We included 13 additional variables that measure one of the four quantitative dimensions of the
+#competitiveness analysis framework used in this report. The variables included are: stock of Greenfield
+#foreign direct investment (FDI) between 2009 and 2015 (traded clusters), stock of Greenfield FDI per
+#capita between 2009 and 2015 (traded clusters), and total stock of jobs created by FDI between 2009 and
+#2015 (traded clusters); number of highly cited papers between 2010 and 2013 (innovation), mean citation
+#score between 2010 and 2013 (innovation), total patents between 2008 and 2012 (innovation), and total
+#patents per capita between 2008 and 2012 (innovation); share of the population with tertiary education
+#(talent) and share of the foreign-born population (talent); and number of aviation passengers in 2014
+#(infrastructure), number of aviation passengers per capita in 2014 (infrastructure), and average internet
+#download speed in 2014 (infrastructure).
+
+df <- data.frame(cat=c(1,1,1,2,2,2,3,3,3), v1=c(1:8, na), v2=c(1:8, na), v3=c(1:8, na), v4=c(1:8, na))
+df %>% group_by(cat) %>% summarise_all(funs(m1=mean(., na.rm=TRUE), m2=mean(., na.rm=TRUE), m3=mean(., na.rm=FALSE)))
+
--- js_source/b_main.js
+++ js_source/b_main.js
@@ -7,16 +7,256 @@ gci2016 = {};
 	//containers for local variables and dom references
 	var scope = gci2016;
 	var dom = scope.dom = {};
+	dom.chartContainers = {};
 
-	//directory containing data assets
-	scope.repo = "./data/";
-	//edit this: scope.repo = "directory/on/wordpress";
+	var dotplots = dom.chartContainers.dotplots = {};
+
+	dotplots["group1"] = document.getElementById("group1_dotplot");
+	dotplots["group2"] = document.getElementById("group2_dotplot");
+	dotplots["group3"] = document.getElementById("group3_dotplot");
+	dotplots["group4"] = document.getElementById("group4_dotplot");
+	dotplots["group5"] = document.getElementById("group5_dotplot");
+	dotplots["group6"] = document.getElementById("group6_dotplot");
+	dotplots["group7"] = document.getElementById("group7_dotplot");
+
+	//directory containing app assets -- need to change to "directory/on/wordpress";
+	scope.repo = "./";
 
 	dom.wrap = d3.select("#gci2016wrap");
 	dom.fixedframewrap = d3.select("#gci2016fixedframewrap");
 	dom.fixedframe = d3.select("#gci2016fixedframe");
 	dom.mapwrap = d3.select("#gci2016mapwrap");
 
+
+	d3.json(scope.repo + "data.json", function(error, dat){
+		if(error){
+			return null;
+		}
+
+		for(var e in dotplots){
+			if(dotplots.hasOwnProperty(e)){
+				scope.scroll.register(dotplots[e], dotPlot.call(dotplots[e], e.replace(/[a-z]*/, ""), dat))
+			}
+		}
+	});
+
+	//dotPlot is called with the enclosing dom container element as the this object
+	function dotPlot(id, data){
+		var box = this.getBoundingClientRect();
+		var wrapper = d3.select(this).classed("c-fix",true);
+		try{
+			var width = Math.floor(box.right - box.left);
+			if(width < 300){throw "badWidth"}
+		}
+		catch(e){
+			var width = 320;
+		}
+		var height = 400;
+		var zrange = [-2,4]
+
+		//vars
+		var vars = ["pop_2015", "X2015_nomgdp", "gdp_pc_real2015", "productivity2015", 
+				"compound_growth_realgdp15", "compound_growth_real_gdp_pc15", "compound_growth_emp15", "compound_growth_pop15", 
+				"fdi_pc_09.15", 
+				"share_publications_top_10_10.13", "patents_pc_08.12", "venture_capital_pc06.15", 
+				"passengers_pc_2014", "download_speed2015", 
+				"share_tertiary_education2015"];
+
+		var dat = vars.map(function(D,I,A){
+			var s = data.z.groups.map(function(d,i,a){
+				return {id:d.id.replace(/[a-z]*/,""), val:d[D], var:D}
+			});
+			return s;
+		});
+
+		var x = d3.scaleBand().domain(vars).range([0,100]).round(true).paddingOuter(1).align(0.5);
+		var y = d3.scaleLinear().domain(zrange).range([height, 0]);
+
+		var svg = wrapper.style("width","100%").style("height",height+"px").append("svg").style("height","100%").style("width","100%");
+
+		svg.append("line").attr("x1","0%").attr("x2","100%").attr("y1",y(0)).attr("y2",y(0)).attr("stroke","#dddddd").style("shape-rendering","crispEdges").attr("stroke-width","2px");
+
+
+		var dots = svg.selectAll("g").data(dat).enter().append("g")
+					  .selectAll("circle").data(function(d,i){return d}).enter().append("circle")
+					  .attr("cx",function(d,i){return x(d.var)+"%"})
+					  .attr("cy",function(d,i){return y(d.val)})
+					  .attr("fill","#cccccc")
+					  .attr("r",5);
+
+		var this_group = dots.filter(function(d,i){
+			return d.id == id;
+		}).raise().attr("fill","red");
+
+		return function(){
+			this_group.transition().attr("r",10);
+		};
+
+
+		///OLD BELOW	
+
+
+		var vargroups = [vars.slice(0,4), vars.slice(4,8), vars.slice(8,9), vars.slice(9,12), vars.slice(12,14), vars.slice(14)];
+		var slice_width = 100/vars.length;
+
+		var plotgroups = wrapper.selectAll("div.plotgroup").data(vargroups).enter().append("div").classed("plotgroup c-fix",true)
+			.style("width",function(d,i){
+				return (d.length * slice_width)+"%";
+			});
+
+		var slices = plotgroups.selectAll("div").data(function(d,i){
+			var w = Math.floor(100/d.length) + "%";
+			return d.map(function(d,i,a){
+				var vals = data.z.groups.map(function(e,i,a){
+					return {id:e.id, val:e[d]};
+				});
+				return {l:d, w:w, vals:vals}
+			});
+		}).enter().append("div").classed("plotslice",true).style("width", function(d,i){return d.w}).style("height",height+"px");
+
+		var zrange = [-2,4];
+		
+		var svg = slices.append("svg").style("width","100%").style("height","100%");
+
+
+
+		var dots = svg.selectAll("circle").data(function(d,i){return d.vals}).enter().append("circle")
+							.attr("cx","50%").attr("cy",function(d,i){return y(d.val)}).attr("r",4).attr("fill","#666666");
+
+		var groupCircles = plotgroups.selectAll("circle").filter(function(d,i){
+				return d.id.replace(/[a-z]*/,"") == id+"";
+			}).raise().attr("fill","red");
+
+
+
+		return function(){
+			groupCircles.transition().duration(700).attr("r",8);
+		}; 
+
+
+//old
+		zrange = [-2,9];
+		zrange = [-2,4];
+
+		var xpad = 25;
+
+		
+		
+		
+		var svg = wrapper.append("svg").style("height",height+"px").style("width",width+"px");
+
+		var mg = svg.append("g").selectAll("g").data(data.z.metros).enter().append("g");
+		var gg = svg.append("g").selectAll("g").data(data.z.groups).enter().append("g");
+		
+		/*var metdots = mg.selectAll("circle").data(function(d,i){
+			var D = [];
+			for(var p in d){
+				if(d.hasOwnProperty(p) && !(p in {id:1, label_cluster:1, tot_emp2015:1, X2015_realgdp:1})){
+					D.push({var:p, val:d[p]});
+				}
+			}
+			return D;
+		}).enter().append("circle").style("opacity",0.4);
+
+		metdots.attr("cx", function(d,i){return x(d.val)}).attr("cy", function(d,i){return y(d.var)}).attr("r",2);*/
+
+		var groupdots = gg.selectAll("circle").data(function(d,i){
+			var D = [];
+			for(var p in d){
+				if(d.hasOwnProperty(p) && !(p in {id:1, label_cluster:1, tot_emp2015:1, X2015_realgdp:1})){
+					D.push({var:p, val:d[p]});
+				}
+			}
+			return D;
+		}).enter().append("circle").style("opacity",0.4);
+
+		groupdots.attr("cy", function(d,i){return y(d.val)}).attr("cx", function(d,i){return x(d.var)}).attr("r",2);
+
+		var titles = svg.selectAll("text").data(vars).enter()
+						.append("text").attr("y",y(0)).attr("x",function(d,i){return x(d)-5})
+						.attr("text-anchor","middle")
+						.text(function(d,i){return d}).style("font-size","13px");
+
+		svg.append("line").attr("x1",x(0)).attr("x2",x(0)).attr("y1",0).attr("y2",height).attr("stroke","#dddddd");
+
+		var activate = function(){
+			svg.append("text").text(id).attr("x",100).attr("y",20).style("opacity",0).transition().duration(500).style("opacity",1);
+			console.log("activate "+ id);
+
+			gg.filter(function(d,i){
+				return d.label_cluster+"" == id+"";
+			}).transition().selectAll("circle").attr("fill","red").attr("r",8);
+		}
+		return activate;
+	}
+
+	//register arbitrary scroll events
+	scope.scroll = {}
+	scope.scroll.count = 0;
+	scope.scroll.listeners = {};
+
+	scope.scroll.register = function(element, onview, onexit){
+		var getBox = function(){
+			try{
+				var box = element.getBoundingClientRect();
+			}
+			catch(e){
+				var box = null;
+			}
+			return box;
+		}
+
+		var id = "event" + (++scope.scroll.count);
+		
+		scope.scroll.listeners[id] = {
+			activated:false,
+			get_box:getBox,
+			listener:onview
+		}
+
+		activateListeners(); //in case one is in view from the outset
+
+	}
+
+	//activate must be called as a method of scope.scroll
+	scope.scroll.activate = function(id, window_height){
+		if(this.listeners.hasOwnProperty(id)){
+			var listener = this.listeners[id];
+			if(listener.activated){
+				//already activated -- no op
+				return null;
+			}
+			else{
+				var box = listener.get_box();
+				if(box===null){
+					return null;
+				}
+				else{
+					var top = box.top;
+					var bottom = box.bottom;
+					var middle = top + ((bottom-top)/2)
+					if(middle - window_height < -50 && box.bottom > 100){
+						listener.activated = true;
+						listener.listener();
+						delete this.listeners[id]; //remove to lower cost of scroll event
+					}
+				}
+			}
+		}
+	}
+
+	//what if innerHeight isn't supported? -- activate all?
+	function activateListeners(){
+		var h = window.innerHeight;
+		for(var id in scope.scroll.listeners){
+			scope.scroll.activate(id, h);
+		}		
+	}
+
+	window.addEventListener("scroll", activateListeners);
+
+
+	//////////////////////////////////////////////////////////////////////////
 	return null;
 	
 
